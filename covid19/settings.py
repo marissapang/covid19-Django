926cca4c8c68592513ef00b84c2928af7bdf96cd
Marissa <marissapang@live.com>
2020-03-29 15:49:58 -0400

    Add allowed hosts = all on settings

--- covid19/settings.py
+++ covid19/settings.py
@@ -133,4 +133,6 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+ALLOWED_HOSTS = ['*']
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
