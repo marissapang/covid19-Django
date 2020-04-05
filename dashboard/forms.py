3b8ab297b1740bf22950308c5d3c8dd138af3be4
Marissa <marissapang@live.com>
2020-04-04 21:42:40 -0400

    added popup filter for user dashboard

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -10,3 +10,17 @@ class SignUpForm(UserCreationForm):
 	class Meta: 
 		model = User
 		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
+
+import pandas as pd
+
+df = pd.read_csv("trends/data/confirmed_cases.csv")
+df = df.groupby('Country')["Num_Confirmed"].agg("sum")
+unique_countries = df.index
+country_choices = list(zip(unique_countries, unique_countries))
+country_choices = [('Global','Global')]+country_choices
+
+class UpdateDashboardCountryForm(forms.Form):
+	countries = forms.MultipleChoiceField(choices = country_choices, label = "Select Countries", widget=forms.CheckboxSelectMultiple)
+
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+import pandas as pd
+from .forms import UpdateDashboardCountryForm
+
+df = pd.read_csv("trends/data/confirmed_cases.csv")
+df = df.groupby('Country').agg("sum")
+unique_countries = df.index
+country_choices = list(zip(unique_countries, unique_countries))
+country_choices = [('Global','Global')]+country_choices
 
 def index(request):
-	test = 1234
-	context = {'test_variable': test}
+	if request.user.is_authenticated == True:
+		username = request.user.username
+		# if request.session.has_key(username):
+		# 	user_session = requestion.session['username']
+		# else: 
+		# 	user_session = {}
+		# 	countries = request.GET.get('dashboard_country_filter')
+
+		dashboard_country_filter_form = UpdateDashboardCountryForm()
+
+		
+		new_list = []
+		print(request.session.has_key(username))
+		request.session['username'] = new_list
+		context={'dashboard_country_filter_form' : dashboard_country_filter_form}
+	else:
+		context = {}
 	return render(request, 'dashboard-index.html', context)
+	
 
 
