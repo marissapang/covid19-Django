039e4594ddbe30968fcf8dce691599843a331f7a
Marissa <1995marissapsy@live.com>
2020-04-06 16:52:58 -0400

    added default checked options for dashboard's country filter

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,26 +10,30 @@ country_choices = [('Global','Global')]+country_choices
 
 def index(request):
 	if request.user.is_authenticated == True: # we only show content if the user is logged in
+		print("first if for authentication")
 		username = request.user.username
 
-		country_selections = []
-
 		##### POP UP FORM #####
-		dashboard_country_filter_form = UpdateDashboardCountryForm()
+		#dashboard_country_filter_form = UpdateDashboardCountryForm()
 		if request.method == "POST":
+			print("request method is post")
 			dashboard_country_filter_form = UpdateDashboardCountryForm(request.POST)
 			if dashboard_country_filter_form.is_valid():
 				country_selections = dashboard_country_filter_form.cleaned_data.get('countries') # this is a list object
-
 				# add country selections to the username's dictionary
 				if request.session.has_key(username): 
 					request.session[username]['country_selections'] = country_selections
 				else: # if user do not already exists, we can just asign a dictionary object
 					request.session[username] = {"country_selections": country_selections}
-
+				request.session.modified = True # save session change
 		else: # if method is not post we just have to generate the form
-			dashboard_country_filter_form = UpdateDashboardCountryForm()			
-		context={'dashboard_country_filter_form' : dashboard_country_filter_form, 'test_list' : ['Global', 'Burma', 'Colombia', 'Brazil']}
+			print("request method is not post")
+			country_selections = request.session.get(username)['country_selections']
+			print("non post country selections")
+			print(country_selections)
+			dashboard_country_filter_form = UpdateDashboardCountryForm(initial={'countries': country_selections})		
+		
+		context={'dashboard_country_filter_form' : dashboard_country_filter_form, 'test_list' : ["Global", "Burma"]}
 	
 
 
