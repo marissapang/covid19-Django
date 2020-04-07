757a4140eecf063b13ba520b10ae3ae5f2673098
Marissa <1995marissapsy@live.com>
2020-04-06 21:31:07 -0400

    removed import request lines

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-import requests
-import request
 
 class SignUpForm(UserCreationForm):
 	first_name = forms.CharField(max_length=50, required=False, help_text='Optional.')
