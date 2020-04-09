7200a1e2c24f21f902922ccaaed185d530bb89e4
Marissa <1995marissapsy@live.com>
2020-04-09 12:05:33 -0400

    combine country & state data code

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -18,7 +18,7 @@ df = pd.read_csv("trends/data/confirmed_cases.csv")
 df = df.groupby('Country')["Num_Confirmed"].agg("sum")
 unique_countries = df.index
 country_choices = list(zip(unique_countries, unique_countries))
-country_choices = [('Global','Global')]+country_choices
+country_choices = [('None', 'None')]+[('Global','Global')]+country_choices
 
 df = pd.read_csv("trends/data/confirmed_cases.csv")
 df = df[df['Country'].isin(['Australia', 'Canada', 'United States', 'China'])]
@@ -33,7 +33,10 @@ for country in countries_w_state_level_data:
 	unique_states = [(country, tuple(zip(unique_states, unique_states)))]
 	country_state_list += unique_states
 
-state_choices = tuple(country_state_list)
+
+none_list = [("", (("None", "None"),))]
+
+state_choices = tuple(none_list + country_state_list)
 
 
 class UpdateDashboardCountryForm(forms.Form):
--- dashboard/views.py
+++ dashboard/views.py
@@ -21,10 +21,12 @@ def index(request):
 			if dashboard_country_filter_form.is_valid():
 				country_selections = dashboard_country_filter_form.cleaned_data.get('countries') # this is a list object
 				# add country selections to the username's dictionary
+				if country_selections == ['None']:
+					country_selections = []
 				current_profile.dashboard_countries = str(country_selections)
 				current_profile.save() # save changes to profie attribute
 			else: 
-				country_selections = []
+				country_selections = ast.literal_eval(current_profile.dashboard_countries)
 				current_profile.dashboard_countries = str(country_selections)
 				current_profile.save() # save changes to profie attribute
 		else: # if method is not post we just have to generate the form
@@ -38,12 +40,13 @@ def index(request):
 			dashboard_state_filter_form = UpdateDashboardStateForm(request.POST)
 			if dashboard_state_filter_form.is_valid():
 				state_selections = dashboard_state_filter_form.cleaned_data.get('states') # this is a list object
-				print(state_selections)
+				if state_selections == ['None']:
+					state_selections = []
 				# add state selections to the username's dictionary
 				current_profile.dashboard_states = str(state_selections)
 				current_profile.save() # save changes to profie attribute
 			else: 
-				state_selections = []
+				state_selections = ast.literal_eval(current_profile.dashboard_states)
 				current_profile.dashboard_states = str(state_selections)
 				current_profile.save() # save changes to profie attribute
 		else: # if method is not post we just have to generate the form
@@ -53,100 +56,77 @@ def index(request):
 
 
 		##### COUNTRY DATA SERIES STARTS #####
+		country_dict = {i : "country" for i in country_selections}
+		state_dict = {i : "state" for i in state_selections}
+		country_dict.update(state_dict)
+		region_dict = country_dict #created dicitionary of region names and whether they're countries or states
+		region_selections = country_selections + state_selections
+		
 		df = pd.read_csv("trends/data/confirmed_cases.csv")
 		dates = list(df['Date'].unique())
 		output_df = pd.DataFrame(dates, columns=["Date"])
-
-		for country in country_selections: 
-
-			if country != "Global": # filter to get country-level data
-				country_data = df[df['Country']==country]
-			else: 
-				country_data = df
-
-			country_data = country_data.groupby(['Date'])["Num_Confirmed"].agg("sum").reset_index(name="Num_Confirmed")
-			country_data = country_data.fillna(0)
-			country_data = country_data.rename(columns={"Num_Confirmed" : country})
-			output_df = output_df.merge(country_data, on="Date", how="left")
-
 		output_dict = {}
-		output_country_list = []
+		output_region_list = []
+		
 
-
-		for i in range(0, 6):
-			if len(country_selections) > i:
-				key = "country_"+str(i) 
-				output_dict[key] = list(output_df[country_selections[i]])
-				output_country_list += [[country_selections[i]]]
+		for region_name, region_type in region_dict.items(): 
+			if region_name == "Global":
+				region_data = df
+			elif region_type == "country":
+				region_data = df[df['Country']==region_name]
+			elif region_type == "state":
+				region_data = df[df['State']==region_name]
 			else:
-				key = "country_"+str(i) 
-				output_dict[key] = []
-				output_country_list += [[""]]
-		##### COUNTRY DATA SERIES ENDS #####
+				region_data="ERROR"
+
+			region_data = region_data.groupby(['Date'])["Num_Confirmed"].agg("sum").reset_index(name="Num_Confirmed")
+			region_data = region_data.fillna(0)
+			region_data = region_data.rename(columns={"Num_Confirmed" : region_name})
+			output_df = output_df.merge(region_data, on="Date", how="left")
 
 
-		##### STATE DATA SERIES STARTS #####
-		df = pd.read_csv("trends/data/confirmed_cases.csv")
-		dates = list(df['Date'].unique())
-		output_df = pd.DataFrame(dates, columns=["Date"])
-
-		for state in state_selections: 
-			state_data = df[df['State']==state]
-
-			state_data = state_data.groupby(['Date'])["Num_Confirmed"].agg("sum").reset_index(name="Num_Confirmed")
-			state_data = state_data.fillna(0)
-			state_data = state_data.rename(columns={"Num_Confirmed" : state})
-			output_df = output_df.merge(state_data, on="Date", how="left")
-
-		output_state_list = []
-
-		for i in range(0, 6):
-			if len(state_selections) > i:
-				key = "state_"+str(i) 
-				output_dict[key] = list(output_df[state_selections[i]])
-				output_state_list += [[state_selections[i]]]
+		for i in range(0, 12):
+			if len(region_dict) > i:
+				key = "region_"+str(i) 
+				output_dict[key] = list(output_df[region_selections[i]])
+				output_region_list += [[region_selections[i]]]
 			else:
-				key = "state_"+str(i) 
+				key = "region_"+str(i) 
 				output_dict[key] = []
-				output_state_list += [[""]]
-
-		print(output_state_list)
-
-
-		##### STATE DATA SERIES ENDS #####
+				output_region_list += [[""]]
+		# ##### COUNTRY DATA SERIES ENDS #####	
 
 		context={
 			'tab' : 'dashboard',
 			'dashboard_country_filter_form' : dashboard_country_filter_form,
 			'dashboard_state_filter_form' : dashboard_state_filter_form,
 			"dates": dates,
-			"country_1": output_dict['country_0'],
-			"country_2": output_dict['country_1'],
-			"country_3": output_dict['country_2'],
-			"country_4": output_dict['country_3'],
-			"country_5": output_dict['country_4'],
-			"country_6": output_dict['country_5'],
-            "country_1_name": output_country_list[0],
-            "country_2_name": output_country_list[1],
-            "country_3_name": output_country_list[2],
-            "country_4_name": output_country_list[3],
-            "country_5_name": output_country_list[4],
-            "country_6_name": output_country_list[5],
-            "state_1": output_dict['state_0'],
-            "state_2": output_dict['state_1'],
-            "state_3": output_dict['state_2'],
-            "state_4": output_dict['state_3'],
-            "state_5": output_dict['state_4'],
-            "state_6": output_dict['state_5'],
-            "state_1_name": output_state_list[0],
-            "state_2_name": output_state_list[1],
-            "state_3_name": output_state_list[2],
-            "state_4_name": output_state_list[3],
-            "state_5_name": output_state_list[4],
-            "state_6_name": output_state_list[5],
+			"region_1": output_dict['region_0'],
+			"region_2": output_dict['region_1'],
+			"region_3": output_dict['region_2'],
+			"region_4": output_dict['region_3'],
+			"region_5": output_dict['region_4'],
+			"region_6": output_dict['region_5'],
+			"region_7": output_dict['region_6'],
+			"region_8": output_dict['region_7'],
+			"region_9": output_dict['region_8'],
+			"region_10": output_dict['region_9'],
+			"region_11": output_dict['region_10'],
+			"region_12": output_dict['region_11'],
+			"region_1_name": output_region_list[0],
+			"region_2_name": output_region_list[1],
+			"region_3_name": output_region_list[2],
+			"region_4_name": output_region_list[3],
+			"region_5_name": output_region_list[4],
+			"region_6_name": output_region_list[5],
+			"region_7_name": output_region_list[6],
+			"region_8_name": output_region_list[7],
+			"region_9_name": output_region_list[8],
+			"region_10_name": output_region_list[9],
+			"region_11_name": output_region_list[10],
+			"region_12_name": output_region_list[11],
 		}
 
-		#print(context)
 
 	else: # if user is not logged in we show nothing
 		context = {}
